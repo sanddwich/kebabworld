b0f2491832c1352be3f69fac7bd77b86ae86dd93
denristun <58968932+denristun@users.noreply.github.com>
2020-09-29 11:16:45 +0400

    11
    11

--- src/Layouts/LayoutsComponents/Footer/Footer.tsx
+++ src/Layouts/LayoutsComponents/Footer/Footer.tsx
@@ -8,9 +8,14 @@ interface FooterState {}
 
 class Footer extends React.Component<FooterProps, FooterState> {
   render() {
-    return(
-      <Container fluid className="Footer">
-        Footer
+    return (
+      <Container fluid className="footer">
+        <div className="footer__copyright text-center ">
+          Сайт разработан -{' '}
+          <a className="footer__copyright__link " href="https://deedesign.ru/">
+            deedesign
+          </a>
+        </div>
       </Container>
     )
   }
