8d5492ea612666eedbcc7ff28e21dc3fa52a9888
denristun <58968932+denristun@users.noreply.github.com>
2020-09-29 11:19:24 +0400

    11
    11

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import AdditionalFeature from './AdditionalFeature/AdditionalFeature'
 import DishMenu from './DishMenu/DishMenu'
+import Map from './Map/Map'
 import './MainPage.scss'
 
 interface MainPageProps {}
 
 interface MainPageState {}
 
-const background: string = "img/fon/bg.png"
+const background: string = 'img/fon/bg.png'
 
 class MainPage extends React.Component<MainPageProps, MainPageState> {
   render() {
-    return(
-      <Container fluid className="MainPage" style={{backgroundImage: `url(${background})`}}>
+    return (
+      <Container fluid className="MainPage" style={{ backgroundImage: `url(${background})` }}>
         <DishMenu />
         <AdditionalFeature />
+        <Map />
       </Container>
     )
   }
